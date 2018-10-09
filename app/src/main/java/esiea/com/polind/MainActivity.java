9c99eafa2ff9341d70ed9bb880538852c4cd3ab6
Alennir <polo92270@hotmail.fr>
2018-10-09 13:54:28 +0200

    Initial commit

---
+++ app/src/main/java/esiea/com/polind/MainActivity.java
@@ -0,0 +1,13 @@
+package esiea.com.polind;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
